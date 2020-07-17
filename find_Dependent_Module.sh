852fc95d38e46234533f8669c0892e048e34a2e1
Mr.jiang <37299204+jjc521@users.noreply.github.com>
2020-07-17 10:13:55 +0800

    Update find_Dependent_Module.sh

--- find_Dependent_Module.sh
+++ find_Dependent_Module.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-FILE=`find ~/Go_WorkSpace/src/github.com/cockroachdb/cockroach/pkg/sql -name "*.go"`
+FILE=`find ~/Go_WorkSpace/src/github.com/cockroachdb/cockroach/pkg/sql -name "*.go"`  #路径换成要匹配文件夹的命令
 
 for file in $FILE
 do
-        sed -ne '/^import (/,/)$/p' $file >> temp2.txt    #匹配形如 "import (.....) 多行匹配,"
+#匹配形如 "import (.....) 多行匹配"
+#匹配两个关键字之间的行 格式： sed -n '/^adm/,/^mysql/p' $为结尾
+        sed -ne '/^import (/,/)$/p' $file >> temp2.txt    
         sed -n '/import [a-z]* "/p' $file >> temp2.txt
         sed -n '/package /p' $file >> temp3.txt
 done
