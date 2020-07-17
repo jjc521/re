cf59b5b3eaed477314544161528030f1c7ef2807
Mr.jiang <37299204+jjc521@users.noreply.github.com>
2020-07-17 13:37:25 +0800

    Update find_Dependent_Module.sh

--- find_Dependent_Module.sh
+++ find_Dependent_Module.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-FILE=`find ~/Go_WorkSpace/src/github.com/cockroachdb/cockroach/pkg/sql -name "*.go"`  #路径换成要匹配文件夹的命令
-
+#获得SQL层的依赖项
+ 
+FILE=`find ~/Go_WorkSpace/src/github.com/cockroachdb/cockroach/pkg/sql -name "*.go"`    #找到查询文件
+ 
 for file in $FILE
 do
-#匹配形如 "import (.....) 多行匹配"
-#匹配两个关键字之间的行 格式： sed -n '/^adm/,/^mysql/p' $为结尾
-        sed -ne '/^import (/,/)$/p' $file >> temp2.txt    
-        sed -n '/import [a-z]* "/p' $file >> temp2.txt
-        sed -n '/package /p' $file >> temp3.txt
+          sed -n '/^import (/,/)$/p' $file >> temp2.txt           #匹配多行导入
+          sed -n '/import [a-z]* "/p' $file >> temp2.txt          #匹配单行导入
+          sed -n '/package /p' $file >> temp3.txt                 #匹配pacakge
 done
+  
+ cat temp2.txt | cut -d '"' -f2 | sed -n '/^[a-z|A-Z]/p' | sed -n '/[a-z|A-Z|0-9]$/p' > temp1.txt        #获取 “” 内的内容
+ sort -k2n temp1.txt | sed '$!N; /^\(.*\)\n\1$/!P; D' > temp.txt         # 去除重复的行
+ 
+ rm temp1.txt temp2.txt         #删除中间文件
+
